2b984fb8229ea0da9d928859f30517a2ab8edfdc
Artem Pashintsev <d1garch@gmail.com>
2022-11-14 22:05:50 +0300

    First commit

---
+++ src/main/java/ru/pashintsev/App.java
@@ -0,0 +1,14 @@
+package ru.pashintsev;
+
+/**
+ * @ Artem Pashintsev
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int[] arr = {1,2,26,27,150,18279};
+        System.out.println(Converter.columnToStringConvert(arr));
+    }
+}
---
+++ src/main/java/ru/pashintsev/Converter.java
@@ -0,0 +1,40 @@
+package ru.pashintsev;
+
+public class Converter {
+
+    public static String columnToStringConvert(int[] columnIndexes) {
+
+        String result = "";
+
+        if(columnIndexes.length == 0) {
+            result = result + "Не введено ни одного значения\n";
+            return result;
+        }
+
+        for(int item : columnIndexes) {
+
+            if (item <= 0) {
+                result = result + "Индекс меньше или равен нулю. Конвертация не возможна.\n";
+                continue;
+            }
+
+            String column = "";
+            int columnIndex = item;
+            columnIndex--;
+
+            do {
+                if (column.length() > 0) {
+                    columnIndex--;
+                }
+                column = ((char) (columnIndex % 26 + (int) 'A')) + column;
+                columnIndex = (columnIndex - columnIndex % 26) / 26;
+            } while (columnIndex > 0);
+
+            result = result + item + ":" + column + "\n";
+        }
+
+        return result;
+    }
+
+
+}
